dc3d7a248019ac7d2ef04c90bc729b4550538ca4
mrphoenixUz <fenixqaqnus2005@gmail.com>
2024-12-05 12:27:47 +0500

    Everything is changed!

--- src/js/html-selection.js
+++
@@ -1,5 +0,0 @@
-const elTodosParent = document.getElementById('todosParent')
-const elTodoTemplate = document.getElementById('todoTemplate')
-const elTodoForm = document.getElementById('todoForm')
-
-export { elTodosParent, elTodoTemplate, elTodoForm }
